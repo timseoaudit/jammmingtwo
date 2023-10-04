d5e8ad94552cf0f9f92eb906283f8a762d90ecd8
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-10-04 05:18:25 +0100

    add all objectives

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,25 @@
+import React, { useState, useCallback } from 'react';
+import './SearchBar.css';
+
+const SearchBar = (props) => {
+    const [term, setTerm] = useState('');
+    
+    const handleTermChange = useCallback((event) => {
+        setTerm(event.target.value);
+    }, []);
+
+    const search = useCallback(() => {
+        props.onSearch(term);
+    }, props.onSearch, [term]);
+
+    return (
+        <div className="SearchBar">
+            <input placeholder="Enter A Song, Album, or Artist" onChange={handleTermChange}/>
+            <button className="SearchButton" onClick={search}>
+            SEARCH
+            </button>
+        </div>
+    );
+}
+
+export default SearchBar;
---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tracklist from '../Tracklist/Tracklist';
+import './SearchResults.css';
+
+const SearchResults = (props) => {
+    return (
+        <div className="SearchResults">
+            <h2>Results</h2>
+            <Tracklist
+            tracks={props.searchResults}
+            onAdd={props.onAdd}
+            isRemoval={false}
+            />
+        </div>
+    );
+};
+
+export default SearchResults;
